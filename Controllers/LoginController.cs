4002b9735a2c24841f8d3d0cddc087f6794f51b2
joaogil433 <2025187956@my.ipleiria.pt>
2026-05-18 16:40:36 +0100

    Hash da password

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,13 +1,25 @@
-﻿using System;
+﻿using Projeto_DA_MDS.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Projeto_DA_MDS.Helpers;
 
 namespace Projeto_DA_MDS.Controllers
 {
     internal class LoginController
     {
+        public static Utilizador Login(string username, string password)
+        {
+            string passwordHash = HashHelper.HashPassword(password);
 
+            using (var db = new IshoppingContext())
+            {
+                return db.Utilizadores
+                    .FirstOrDefault(u => u.Username == username
+                                      && u.Password == passwordHash);
+            }
+        }
     }
 }
